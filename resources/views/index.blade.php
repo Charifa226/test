ee61a106b113a983051d71008226d6f42e6e3748
charifa <denecharifa71@gmail.com>
2022-10-03 19:26:35 +0000

    integration bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+       
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <title>Hello, world!</title>
+    <button class="btn btn-primary">Bootstrap btn</button>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+  </body>
+</html>
